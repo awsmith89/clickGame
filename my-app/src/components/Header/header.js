89c626beb9ed6f10791f3fed9a227f5b33c34c4e
Andy Smith <andysmith@er-10-129-2-251.wireless.umn.edu>
2018-07-02 17:16:35 -0500

    mostly working

---
+++ my-app/src/components/Header/header.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./header.css";
+
+const Header = () => (
+  <header className = "jumbotron">
+    <h1>Cartoon Memory Game</h1>
+    <h3>Avoid clicking the same image more than once</h3>
+  </header>
+);
+
+export default Header;
